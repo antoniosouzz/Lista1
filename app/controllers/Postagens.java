9f7c25f99cd0f5afba85ee5e338170600b37beb1
Antonio Vitor <itmaiavitor@gmail.com>
2025-05-15 16:46:15 -0300

    Versão quase final (falta deixar bonito)

--- app/controllers/Postagens.java
+++ app/controllers/Postagens.java
@@ -12,10 +12,30 @@ public class Postagens extends Controller{
 		render(listagem);
 	}
 	
+	public static void form() {
+		render();
+	}
+	
 	public static void salvar(Postagem p) {
-		if (p != null) {
-	        p.save();
+		if (p.titulo != null) {
+			p.titulo = p.titulo.toUpperCase();
 	    }
+		
+		if (p.conteudo != null) {
+			p.conteudo = p.conteudo.toUpperCase();
+	    }
+		
+		if (p.autor != null) {
+			p.autor = p.autor.toUpperCase();
+	    }
+		
+		p.save();
+		listar();
+	}
+	
+	public static void remover(long id) {
+		Postagem postagem = Postagem.findById(id);
+		postagem.delete();
 		listar();
 	}
 
